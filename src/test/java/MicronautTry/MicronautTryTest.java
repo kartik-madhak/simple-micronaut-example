fc87b5deaeedbe0d49a98599dc673adfe9421376
Kartik <58913047+kartik-madhak@users.noreply.github.com>
2022-02-09 01:16:48 +0530

    Initial commit

---
+++ src/test/java/MicronautTry/MicronautTryTest.java
@@ -0,0 +1,21 @@
+package MicronautTry;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautTryTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
